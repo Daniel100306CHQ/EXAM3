e266e58fd4ecdcce962da45087f47a2080e6a8d2
Daniel100306CHQ <83975559+Daniel100306CHQ@users.noreply.github.com>
2021-10-11 12:30:10 -0500

    Add files via upload

---
+++ js/yaba.js
@@ -0,0 +1,45 @@
+let song1 = document.getElementById("song1");
+let primero = document.getElementById("primero");
+primero.addEventListener("click", () =>{
+  song1.play();
+})
+let song2 = document.getElementById("song2");
+let segundo = document.getElementById("segundo");
+segundo.addEventListener("click", () =>{
+  song2.play();
+})
+let song3 = document.getElementById("song3");
+let tercero = document.getElementById("tercero");
+tercero.addEventListener("click", () =>{
+  song3.play();
+})
+let song4 = document.getElementById("song4");
+let cuarto = document.getElementById("cuarto");
+cuarto.addEventListener("click", () =>{
+  song4.play();
+})
+let song5 = document.getElementById("song5");
+let quinto = document.getElementById("quinto");
+quinto.addEventListener("click", () =>{
+  song5.play();
+})
+let song6 = document.getElementById("song6");
+let sexto = document.getElementById("sexto");
+sexto.addEventListener("click", () =>{
+  song6.play();
+})
+let song7 = document.getElementById("song7");
+let septimo = document.getElementById("septimo");
+septimo.addEventListener("click", () =>{
+  song7.play();
+})
+let song8 = document.getElementById("song8");
+let octavo = document.getElementById("octavo");
+octavo.addEventListener("click", () =>{
+  song8.play();
+})
+let song9 = document.getElementById("song9");
+let noveno = document.getElementById("noveno");
+noveno.addEventListener("click", () =>{
+  song9.play();
+})
